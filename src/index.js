cc73dcca2be5dc0123a9de9b6b41fabe04bc577c
Lachlan Underhill <lachlanu@gmail.com>
2019-03-29 12:46:29 +1000

    Deployed to firebase

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import reduxThunk from 'redux-thunk';
-import reducers from './reducers';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
