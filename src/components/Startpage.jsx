371a6736f8e67c0f8f73a1139ffbe0984512f02d
Lachlan Underhill <lachlanu@gmail.com>
2019-08-18 09:50:52 +1000

    Added background image to startpage

--- src/components/Startpage.jsx
+++ src/components/Startpage.jsx
@@ -4,8 +4,10 @@ import "./styles/startpage.scss";
 export default class Startpage extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="content">start page goes here</div>
+      <div className="startpage-container">
+        <div className="content-wrapper">
+          <div className="content">start page goes here</div>
+        </div>
       </div>
     );
   }
