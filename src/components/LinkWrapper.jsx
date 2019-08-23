f6f09ce44649ac362b7778b65f4b1b227559b615
Lachlan Underhill <lachlanu@gmail.com>
2019-08-23 11:06:31 +1000

    Tidied up auth

---
+++ src/components/LinkWrapper.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+function Links(props) {
+  const link_content = props.content;
+
+  const listItems = link_content.map(link => (
+    <li key={link.url}>
+      <a href={link.url}>{link.name}</a>
+    </li>
+  ));
+
+  return <ul>{listItems}</ul>;
+}
+
+export default class LinkWrapper extends Component {
+  render() {
+    return (
+      <div className="link-wrapper">
+        <div className="wrapper-title">{this.props.title}</div>
+        <div className="wrapper-body col">
+          <Links content={this.props.content} />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/auth/DisplayAuthAction.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { connect } from "react-redux";
+import firebaseConnect from "react-redux-firebase/lib/firebaseConnect";
+import { isLoaded, isEmpty } from "react-redux-firebase/lib/helpers";
+import { compose, withHandlers } from "recompose";
+
+function DisplayAuthAction({ firebase, auth }) {
+  function LoginButton() {
+    return (
+      <button
+        onClick={() => {
+          firebase.login({ provider: "google", type: "popup" });
+        }}
+      >
+        Login
+      </button>
+    );
+  }
+
+  function LogoutButton() {
+    return (
+      <button
+        onClick={() => {
+          firebase.logout();
+        }}
+      >
+        Logout
+      </button>
+    );
+  }
+
+  if (!isLoaded(auth)) {
+    return <div>Loading...</div>;
+  }
+  if (isEmpty(auth)) {
+    return (
+      <div>
+        <p>Auth is empty</p>
+        <LoginButton />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <p>Hey there, {auth.displayName}</p>
+      <LogoutButton />
+    </div>
+  );
+}
+
+const enhance = compose(
+  firebaseConnect(),
+  connect(state => ({
+    auth: state.firebase.auth
+  }))
+);
+export default enhance(DisplayAuthAction);
