3cbe47804bed83b9ec8d93a964cf5aa2c6ff670b
Lachie <lachlanu@gmail.com>
2020-05-09 14:46:43 +1000

    Switched from HOC to hook in link wrapper

--- src/components/startpage/LinkWrapper.jsx
+++ src/components/startpage/LinkWrapper.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { compose, withHandlers } from "recompose";
-import { connect } from "react-redux";
-import { firebaseConnect } from "react-redux-firebase";
+import { connect, useSelector } from "react-redux";
+import {
+  firebaseConnect,
+  useFirebaseConnect,
+  isLoaded,
+  isEmpty,
+} from "react-redux-firebase";
 import { get } from "lodash";
 import PropTypes from "prop-types";
 
@@ -21,65 +26,83 @@ function Links(props) {
   );
 }
 
-function LinkWrapper({ linkCategories }) {
-  let edit_mode = false;
+export default function LinkWrapper(props) {
+  useFirebaseConnect(`users/${props.userId}/links`);
 
-  return (
-    <>
-      {linkCategories &&
-        Object.keys(linkCategories).map((category_name) => {
-          const category = linkCategories[category_name];
-
-          // Check for color variable in category entry and assign to div style
-          // If found, remove from category object
-          let divStyle;
-          if (category._color) {
-            divStyle = {
-              backgroundColor: category._color,
-            };
-            delete category._color;
-          } else {
-            divStyle = {};
-          }
-
-          return (
-            <div className="link-wrapper" key={category_name}>
-              <div className="wrapper-title" style={divStyle}>
-                {category_name}
-              </div>
-              <div className="wrapper-body col">
-                <Links content={category} edit={edit_mode} />
-              </div>
-            </div>
-          );
-        })}
-    </>
+  let linkCategories = useSelector(
+    ({
+      firebase: {
+        ordered: { users },
+      },
+    }) => users && users[props.userId].links
   );
+
+  let edit_mode = false;
+  if (isEmpty(linkCategories)) {
+    return <div>Loading...</div>;
+  } else {
+    let categories = {};
+    linkCategories.forEach((category, index) => {
+      categories[category.key] = category.value;
+    });
+
+    return (
+      <>
+        {categories &&
+          Object.keys(categories).map((category_name) => {
+            const category = categories[category_name];
+
+            // Check for color variable in category entry and assign to div style
+            // If found, remove from category object
+            let divStyle;
+            if (category._color) {
+              divStyle = {
+                backgroundColor: category._color,
+              };
+              delete category._color;
+            } else {
+              divStyle = {};
+            }
+
+            return (
+              <div className="link-wrapper" key={category_name}>
+                <div className="wrapper-title" style={divStyle}>
+                  {category_name}
+                </div>
+                <div className="wrapper-body col">
+                  <Links content={category} edit={edit_mode} />
+                </div>
+              </div>
+            );
+          })}
+      </>
+    );
+  }
 }
 
 LinkWrapper.propTypes = {
   linkCategories: PropTypes.object,
 };
 
-export default compose(
-  firebaseConnect((props) => [
-    `users/${props.userId}/links`, // sync /links/userId from firebase into redux
-  ]),
+// export default compose(
+//   firebaseConnect((props) => [
+//     `users/${props.userId}/links`, // sync /links/userId from firebase into redux
+//   ]),
 
-  connect((state, props) => ({
-    linkCategories: get(state.firebase.data, `users.${props.userId}.links`),
-  })),
+//   connect((state, props) => ({
+//     linkCategories: get(state.firebase.data, `users.${props.userId}.links`),
+//   })),
 
-  withHandlers({
-    addLink: (props) => () => {
-      const userId = props.auth.uid;
-      const sampleTodo = { text: "Sample", done: false };
-      return props.firebase.push(`links/${userId}`, sampleTodo);
-    },
+//   withHandlers({
+//     addLink: (props) => () => {
+//       const userId = props.auth.uid;
+//       const sampleTodo = { text: "Sample", done: false };
+//       return props.firebase.push(`links/${userId}`, sampleTodo);
+//     },
 
-    removeLink: (props) => (key) => () => {
-      const userId = props.auth.uid;
-      return props.firebase.remove(`links/${userId}/${key}`);
-    },
-  })
-)(LinkWrapper);
+//     removeLink: (props) => (key) => () => {
+//       const userId = props.auth.uid;
+//       return props.firebase.remove(`links/${userId}/${key}`);
+//     },
+//   })
+// )(LinkWrapper);
