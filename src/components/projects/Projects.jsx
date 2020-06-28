eee4d7cb00ed39ac7c399fd329e2bbaa210525f5
Lachie <lachlanu@gmail.com>
2020-06-28 18:22:53 +1000

    Dynamically update project list

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,12 +1,42 @@
-import React from "react";
-import ProjectPreview from "./ProjectPreview";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Projects() {
+  const initialProjects = [
+    {
+      name: "project_a",
+      name_display: "Project A",
+      content: "This project is about this",
+    },
+    {
+      name: "project_b",
+      name_display: "Second Project",
+      content: "This project is about another thing",
+    },
+  ];
+
+  const [projects, setProjects] = useState(initialProjects);
+  const [preview, setPreview] = useState({ name_display: "Projects" });
+
+  function ProjectList() {
+    const listItems = projects.map((project) => (
+      <li
+        key={project.name}
+        onMouseEnter={() => setPreview(project)}
+        onMouseLeave={() => setPreview({ name_display: "Projects" })}
+      >
+        <Link to={`projects/${project.name}`}>{project.name_display}</Link>
+      </li>
+    ));
+
+    return <ul className="project-list">{listItems}</ul>;
+  }
+
   return (
     <>
       <div className="projects-content">
-        <h1>Projects</h1>
-        <ProjectPreview />
+        <h1>{preview.name_display}</h1>
+        <ProjectList />
       </div>
     </>
   );
