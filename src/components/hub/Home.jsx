c85e76dab61f54f5d42b88e0ca16fd432cd8b9f3
Lachie <lachlanu@gmail.com>
2020-06-28 23:39:00 +1000

    Added project link to landing page

--- src/components/hub/Home.jsx
+++ src/components/hub/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles.scss";
+import { Link } from "react-router-dom";
 
 export default function Home(props) {
   return (
@@ -8,6 +9,7 @@ export default function Home(props) {
         <h1 className="hero-text" data-shadow="hello!">
           hello!
         </h1>
+        <Link to="projects">Check out my projects</Link>
       </div>
     </>
   );
