5bea7bd8802b254a4993817bc53cbe4b43f7ab69
Lachie <lachlanu@gmail.com>
2020-06-28 17:21:59 +1000

    Fancy home page hero text

--- src/components/hub/Home.jsx
+++ src/components/hub/Home.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import "../styles.scss";
-import Header from "../nav/Header";
-import Footer from "../nav/Footer";
 
 export default function Home(props) {
   return (
-    <div className="home-container">
-      <Header />
-      <div className="hero">
-        <h1>Fancy text here</h1>
+    <>
+      <div className="home-container">
+        <h1 className="hero-text" data-shadow="hello!">
+          hello!
+        </h1>
       </div>
-      <Footer />
-    </div>
+    </>
   );
 }
