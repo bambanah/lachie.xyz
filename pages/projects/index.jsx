36d54cd5ea231d86a1ea0c45f6e1d9846c8f8149
Lachie <lachlanu@gmail.com>
2020-06-29 20:30:24 +1000

    View markdown in projects

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -58,7 +58,7 @@ export default function Projects() {
 
   return (
     <Layout>
-      <div className={styles.content}>
+      <div className={styles.container}>
         <ProjectList />
 
         <ProjectPreview />
