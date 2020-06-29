17283573c76ea39bbd3bd62709e161fb22cf331e
Lachie <lachlanu@gmail.com>
2020-06-29 13:23:25 +1000

    Added arrow to project link on landing page

--- src/components/hub/Home.jsx
+++ src/components/hub/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "../styles.scss";
 import { Link } from "react-router-dom";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 export default function Home(props) {
   return (
     <>
@@ -9,7 +12,9 @@ export default function Home(props) {
         <h1 className="hero-text" data-shadow="hello!">
           hello!
         </h1>
-        <Link to="projects">Check out my projects</Link>
+        <Link to="projects">
+          Check out my projects <FontAwesomeIcon icon={faArrowRight} />
+        </Link>
       </div>
     </>
   );
