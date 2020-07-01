37916bd327b6c66563d5b86d3abee6b039879833
Lachlan Underhill <lachlanu@gmail.com>
2020-07-01 18:36:03 +1000

    Placeholder playground text

--- pages/playground/index.jsx
+++ pages/playground/index.jsx
@@ -4,7 +4,11 @@ import styles from "../../components/styles/playground.module.scss";
 function Playground() {
   return (
     <Layout>
-      <h1>This is the playground.</h1>
+      <section className={styles.container}>
+        <h1 className={styles.title}>
+          This is the future home of any random fun things I feel like making.
+        </h1>
+      </section>
     </Layout>
   );
 }
