741d5e19194a29be40a9c5c6b2019f6253297859
Lachlan Underhill <lachlanu@gmail.com>
2020-07-01 19:21:33 +1000

    Added links to footer

--- components/nav/Footer.jsx
+++ components/nav/Footer.jsx
@@ -7,6 +7,14 @@ export default function Footer() {
         <li>
           <a href="https://github.com/Bambanah">GitHub</a>
         </li>
+        <li>
+          <a href="https://linkedin.com/in/lachlan-underhill-7476681a0/">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://www.hackerrank.com/Bambanah">HackerRank</a>
+        </li>
       </ul>
     </footer>
   );
