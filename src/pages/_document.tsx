1f7d0b1f8814be3fb3996f193bd198a19d77d783
Lachlan Underhill <lachlanu@gmail.com>
2022-05-16 15:55:39 +1000

    fix: add display swap to font import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<link
-						href="https://fonts.googleapis.com/css2?family=Righteous&family=Roboto+Mono&family=Roboto+Slab:wght@700&display=optional"
+						href="https://fonts.googleapis.com/css2?family=Righteous&family=Roboto+Mono&family=Roboto+Slab:wght@700&display=swap"
 						rel="stylesheet"
 					/>
 				</Head>
