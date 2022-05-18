5db12a65d41c4a1f72ed5bdb3e3ce7541065216c
Lachlan Underhill <lachlanu@gmail.com>
2022-05-19 01:36:39 +1000

    chore: remove unneeded imports

--- src/templates/layout/layout.tsx
+++ src/templates/layout/layout.tsx
@@ -1,13 +1,11 @@
-import Head from "next/head";
 import React, { ReactNode } from "react";
 import * as Styles from "./layout.styles";
 
 interface Props {
-	title?: string;
 	children: ReactNode;
 }
 
-const BaseLayout: React.FC<Props> = ({ title, children }) => {
+const BaseLayout: React.FC<Props> = ({ children }) => {
 	return (
 		<Styles.Container>
 			<Styles.Section>{children}</Styles.Section>
